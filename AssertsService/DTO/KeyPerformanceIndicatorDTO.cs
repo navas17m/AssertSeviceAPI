6168ebd66b5bebb14d13fb2f522bbfe54a9d3b97
navas <navas17m@gmail.com>
2025-03-02 13:11:11 +0300

    project file commit

---
+++ AssertsService/DTO/KeyPerformanceIndicatorDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace AssertsService.DTO
+{
+    public class KeyPerformanceIndicatorDTO
+    {
+        public int KeyPerformanceIndicatorId { get; set; }     
+       
+        public string KeyPerformanceIndicatorName { get; set; }
+        public string KeyPerformanceIndicatorCategorylName { get; set; }
+        public string? Description { get; set; }
+        public string? Baseline { get; set; }
+        public string? ComingThrough { get; set; }
+    }
+}
