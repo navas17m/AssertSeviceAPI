c753a4d37a0dfdab48d91d4b85e23127cd59fb71
navas <navas17m@gmail.com>
2025-03-11 10:26:34 +0300

    tt

---
+++ AssertsService/Migrations/20250310080401_assert add evidance.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AssertsService.Migrations
+{
+    /// <inheritdoc />
+    public partial class assertaddevidance : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AccidentDescription",
+                table: "AssertRegisters",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Evidence",
+                table: "AssertRegisters",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccidentDescription",
+                table: "AssertRegisters");
+
+            migrationBuilder.DropColumn(
+                name: "Evidence",
+                table: "AssertRegisters");
+        }
+    }
+}
